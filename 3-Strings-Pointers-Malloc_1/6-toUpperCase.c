fad0a3cb48594462c46ec2cfbb1572c22fd67b19
Oliver Melo <contato_limaverde@protonmail.com>
2025-03-31 19:04:07 -0300

    Coded ex 06 of list 03; renamed file to reflect its contents

---
+++ 3-Strings-Pointers-Malloc_1/6-toUpperCase.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* to_upper_case(char original_str[], int len_original_str){
+
+    char* result_str = (char*) malloc(sizeof(char)*len_original_str);
+
+    for (int i; i<len_original_str; i++){
+            
+        if (original_str[i] >= 'a' && original_str[i] <= 'z'){
+
+            result_str[i] = original_str[i] - 32;
+
+        }
+        else {
+
+            result_str[i] = original_str[i];
+
+        }
+
+    }
+
+    return result_str;
+    
+}
+
+int main(){
+
+    char original_str[100], *aux_string, *result_str;
+
+    printf("Escreva seu texto: \n\n");
+    scanf("%s", original_str);
+
+    int len_original_str = strlen(original_str);
+
+    aux_string = (char*) malloc(sizeof(char) * len_original_str);
+
+    for (int i = 0; i < len_original_str; i++) {
+        
+        aux_string[i] = original_str[i];
+
+    }
+
+    result_str = to_upper_case(aux_string, len_original_str);
+
+    for (int i = 0; i < len_original_str; i++) {
+        
+        printf("%c", result_str[i]);
+
+    }
+    
+
+}
