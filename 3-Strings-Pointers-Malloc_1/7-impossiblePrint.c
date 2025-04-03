6f628557788c5942e9b9bd0641276e7fc17a2b5e
Oliver Melo <contato_limaverde@protonmail.com>
2025-04-02 22:57:09 -0300

    Coded a broken 07 exercise of list 03, very hard one, giving up for now. Renamed it to "impossiblePrint.C". Added its function to "useless_prints.c".

--- 3-Strings-Pointers-Malloc_1/7-impossiblePrint.c
+++ 3-Strings-Pointers-Malloc_1/7-impossiblePrint.c
@@ -1,99 +1,6 @@
-void linha2(char carac){
-    
-    for(int i = 0; i<25; i++){printf("%c", carac);}
-    
-}
-
-void desenha_retangulo_solido(int linhas, int colunas, char carac){
-    
-    for (int i=0; i<linhas; i++){
-        for (int j=0; j<colunas; j++){
-            printf("%c", carac);
-        }
-        printf("\n");
-    }
-    
-}
-
-void desenha_retangulo(int linhas, int colunas, char carac){
-    
-    for (int i=0; i<linhas; i++){
-        
-        for (int j=0; j<colunas; j++){
-            if(i==0 || i==linhas-1 || j==0 || j==colunas-1){
-                printf("%c", carac);   
-            }
-            else{
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-    
-}
-
-void desenha_retangulo_preenchido(int linhas, int colunas, char contorno, char preenchimento){
-    
-    for (int i=0; i<linhas; i++){
-        
-        for (int j=0; j<colunas; j++){
-            if(i==0 || i==linhas-1 || j==0 || j==colunas-1){
-                printf("%c", contorno);   
-            }
-            else{
-                printf("%c", preenchimento);
-            }
-        }
-        printf("\n");
-    }
-    
-}
-
-void print_month(int month){
-    switch(month){
-        case 1:
-            printf("Janeiro");
-            return;
-        case 2:
-            printf("Fevereiro");
-            return;
-        case 3:
-            printf("Março");
-            return;
-        case 4:
-            printf("Abril");
-            return;
-        case 5:
-            printf("Maio");
-            return;
-        case 6:
-            printf("Junho");
-            return;
-        case 7:
-            printf("Julho");
-            return;
-        case 8:
-            printf("Agosto");
-            return;
-        case 9:
-            printf("Setembro");
-            return;
-        case 10:
-            printf("Outubro");
-            return;
-        case 11:
-            printf("Novembro");
-            return;
-        case 12:
-            printf("Dezembro");
-            return;
-        default:
-            printf("Não há mês correspondente.");
-            return;
-    }
-
-
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void caixa_com_texto_centralizado(char S[], int N, char C){
 
@@ -134,7 +41,7 @@ void caixa_com_texto_centralizado(char S[], int N, char C){
                         if (largura >= N-qtd_espacos-1) { printf(" "); }
                     }
                 }
-
+    
             }
 
             else if (strlen(S) == N-2){
@@ -161,7 +68,7 @@ void caixa_com_texto_centralizado(char S[], int N, char C){
                 int aux = ((strlen(S) - (N-2)) / 2);
 
                 for (int largura = 0; largura<N; largura++){
-
+    
                     if (largura == 0){ printf("%c", C); } 
 
                     if (resto == 0 && largura >= aux && largura < strlen(S) - aux){
@@ -183,4 +90,17 @@ void caixa_com_texto_centralizado(char S[], int N, char C){
 
         printf("\n");
     }
+}
+
+
+int main(){
+
+    char str[100] = "asdf É O CENTRO asdf";
+    int N = 10;
+    char C = '+';
+
+    caixa_com_texto_centralizado(str, N, C);
+
+    return 0;
+
 }
